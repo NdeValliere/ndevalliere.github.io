4a8d0c2cd753856f2efb9751ffa9797d3a6f3628
Nathalie de Valliere <nathalie.devalliere@gmail.com>
2017-06-18 19:03:14 -0400

    Personal website experiments

--- scripts/scripts-1.js
+++ scripts/scripts-1.js
@@ -1,3 +1,18 @@
+var IH = window.innerHeight,
+    IW = window.innerWidth;
+
+$(".container__black").css({"height": IH, "width": IW/2});
+$(".container__white").css({"height": IH, "width": IW/2});
+
+$('.container__vid').on('mouseenter',function(e){
+    var maxX = $(window).width() - $(this).width();
+    var maxY = $(window).height() - $(this).height();
+    $(this).css({
+        'left':getRandomInt(0, maxX),
+        'top':getRandomInt(0, maxY)
+    });
+});
+
 /**
  * Vars
  */
