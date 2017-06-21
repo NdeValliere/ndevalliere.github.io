0dfd8334f1efe11669c5547e205c390c703f27f5
Nathalie de Valliere <nathalie.devalliere@gmail.com>
2017-06-21 00:14:56 -0400

    tweaking of animation timing

--- scripts/scripts-5.js
+++ scripts/scripts-5.js
@@ -2,17 +2,27 @@ $(document).ready(function(){
 		var selection = document.getElementsByTagName('span');
 
     var timesRun = 0;
-    var interval = setInterval(execute, 5000);
+    var interval = setInterval(execute, 500);
 
     function execute(){
         timesRun += 1;
 
-				console.log('running');
     		var randomSpan = selection[Math.floor(Math.random() * selection.length)];
+
 				randomSpan.setAttribute('class','large');
 
-        if(timesRun === 5){
+        if(timesRun === 100){
             clearInterval(interval);
+        }
+
+        $('.container__project-titles').css({
+            'left': timesRun + '%'
+        });
+
+        if(timesRun > 20) {
+          $('.container__project-titles').css({
+              'left': '-15%'
+          });
         }
      }
 });
@@ -20,13 +30,15 @@ $(document).ready(function(){
 
 $('span, .page__title').on('mouseenter',function(){
     $(this).css({
-        'background-image': 'url("../assets/gifs/' + getRandomInt(1, 39) + '.gif")'
+        'background-image': 'url("../assets/gifs/' + getRandomInt(1, 39) + '.gif")',
+        'color': 'rgba('+ getRandomInt(0,255) + ',' + getRandomInt(0,255) + ',' + getRandomInt(0,255) + ','  + '1)'
     });
 });
 
 $('span, .page__title').on('mouseleave',function(){
     $(this).css({
-        'background-image': 'none'
+        'background-image': 'none',
+        'color': 'black'
     });
 });
 
