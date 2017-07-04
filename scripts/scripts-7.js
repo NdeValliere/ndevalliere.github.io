b900e18a108a5e9a23947bbabf6f913342f39642
Nathalie de Valliere <nathalie.devalliere@gmail.com>
2017-07-03 22:24:45 -0400

    portfolio2.0

---
+++ scripts/scripts-7.js
@@ -0,0 +1,22 @@
+var resize = (function () {
+		var IH = window.innerHeight,
+				IW = window.innerWidth;
+
+		// $(".container__grid").css({"height": IH, "width": IW});
+
+		$(".grid__row").css({"margin": IH / 30});
+
+		$(".grid__rect").css({"height": IH/6, "width": IW*0.09});
+   });
+
+$(document).ready(resize);
+$(window).resize(resize);
+
+$( document ).on( "mousemove", function( event ) {
+	var mX = event.pageX,
+			mY = event.pageY;
+
+			console.log (mX);
+
+	$("body").css({"background": "linear-gradient(" + mX/10 + "deg, rgba(255,235,235,1), rgba(255,255,255,0))"});
+});
