536530a6d264b6baca3b073154e22891fa5cbbed
Nathalie de Valliere <nathalie.devalliere@gmail.com>
2017-07-03 23:54:33 -0400

    prototype 2.1

--- scripts/scripts-7.js
+++ scripts/scripts-7.js
@@ -4,7 +4,25 @@ var resize = (function () {
 
 		$("body").css({"height": IH, "width": IW});
 		$(".grid__row").css({"margin": IH / 30});
-		$(".grid__rect").css({"height": IH/6, "width": IW*0.09});
+		$(".grid__rect").css({"height": IH/6, "width": IW/11});
+
+		$( ".grid__rect" ).mouseover(function() {
+			$(".grid__rect").css({"background-color": "rgba(253,255,253,1)"});
+			$(this).css({"background-color": "rgba(255,255,255,1)",
+									 "box-shadow": "0px 0px 23px 1px rgba(255,234,235,1)",
+									 // "transform": "rotateX(180deg)",
+									 // "-webkit-transform": "rotateX(180deg)",
+									 "width": IW/2.9});
+		});
+
+		$( ".grid__rect" ).mouseout(function() {
+			$(".grid__rect").css({"background-color": "rgba(255,255,255,1)"});
+			$(this).css({"background-color": "rgba(255,255,255,1)",
+									 "box-shadow": "0px 0px 0px 0px rgba(255, 255,255,1)",
+									 // "transform": "rotateX(0deg)",
+									 // "-webkit-transform": "rotateX(0deg)",
+									 "width": IW/11});
+		});
    });
 
 $(document).ready(resize);
@@ -13,8 +31,6 @@ $(window).resize(resize);
 $( document ).on( "mousemove", function( event ) {
 	var mX = event.pageX,
 			mY = event.pageY;
-
-			console.log (mX);
 
 	$("body").css({"background": "linear-gradient(" + mX/10 + "deg, rgba(255,235,235,1), rgba(255,255,255,0))"});
 });
