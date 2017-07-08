6014471fe25a5f6ca4174f4731c8921db5c3c7ff
Nathalie de Valliere <nathalie.devalliere@gmail.com>
2017-07-08 00:10:22 -0400

    hover links

--- scripts/scripts-7.js
+++ scripts/scripts-7.js
@@ -7,15 +7,21 @@ var resize = (function () {
 		$(".grid__rect").css({"height": IH/9, "width": IW/11});
 
 		$( ".grid__rect" ).mouseover(function() {
+			$(this).find(".grid__link").css({"margin-bottom": "50px",
+																			 "transform": "rotate(2deg)",
+																			 "-webkit-transform": "rotate(2deg)",
+																			});
 			$(".grid__rect").css({"background-color": "rgba(254,253,252,1)"});
 			$(this).css({"background-color": "rgba(255,255,255,1)",
 									 "box-shadow": "0px 0px 23px 1px rgba(255,234,235,1)",
-									 // "transform": "rotateX(180deg)",
-									 // "-webkit-transform": "rotateX(180deg)",
 									 "width": IW/3});
 		});
 
 		$( ".grid__rect" ).mouseout(function() {
+			$(this).find(".grid__link").css({"margin-bottom": "10px",
+																			 "transform": "rotate(0deg)",
+																			 "-webkit-transform": "rotate(0deg)",
+																			});
 			$(".grid__rect").css({"background-color": "rgba(255,255,255,1)"});
 			$(this).css({"background-color": "rgba(255,255,255,1)",
 									 "box-shadow": "0px 0px 0px 0px rgba(255, 255,255,1)",
