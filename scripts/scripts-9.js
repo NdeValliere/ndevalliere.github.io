11a87b65b08c6f8ab414233de1bbb2735cf43f0d
Nathalie de Valliere <nathalie.devalliere@gmail.com>
2017-07-18 18:27:12 -0400

    resizing boxes on hover

--- scripts/scripts-9.js
+++ scripts/scripts-9.js
@@ -8,10 +8,20 @@ var resize = (function () {
 
 		$("body").css({"height": IH, "width": IW,
                    "background-size": IW + "px " + IH * 1.1 + "px"});
-    $(".container__grid").css({"transform": "perspective(" + IW + "px) rotateX( -10deg )"});
+    $(".container__grid").css({"transform": "perspective(" + IW*10 + "px) rotateX( -10deg )"});
 		$(".grid__rect").css({"height": IH/5, "width": IW/7});
     $(".grid__rect-inner").css({"height": IH/6.1, "width": IW/7.15,
                                 "margin-top": IH/30});
+
+
+      $( ".grid__rect" ).mouseout(function() {
+        $(".grid__rect").css({"margin-top": 0});
+        $(".grid__row").css({"margin-right": 0,
+                            "margin-left": 0});
+        $(".grid__rect").css({"height": IH/5, "width": IW/7});
+        $(".grid__rect-inner").css({"height": IH/6.1, "width": IW/7.15,
+                                    "margin-top": IH/30});
+      });
    });
 
 $(document).ready(resize);
@@ -26,91 +36,88 @@ $(document).on( "mousemove", function( event ) {
 });
 
 
-var timeout, clicker = $(".grid__rect");
-clicker.mousedown(function(){
-    timeout = setInterval(function(){
-      $(".grid__rect").css({"cursor": "-webkit-grabbing"});
-    }, 50);
-    return false;
-});
-$(document).mouseup(function(){
-    clearInterval(timeout);
-    $(".grid__rect").css({"cursor": "pointer"});
-    return false;
-});
-
-
 $( ".grid__rect" ).mouseover(function() {
   //get nth-child info of hovered element
   var index = $(this).index() + 1;
 
-  $(this).css({"margin-top": "-=175px"});
+  $(this).css({"margin-top": "-175px",
+                "width": "+=200px",
+                "height": "+=200px"});
+  $(this).find(".grid__rect-inner").css({"width": "+=200px",
+                                          "height": "+=200px"});
 
   if (index == 1) {
-    $(this).parent(".grid__row").children(":nth-child(2)").css({"margin-top": "-=75px"});
-    $(this).parent(".grid__row").children(":nth-child(3)").css({"margin-top": "-=25px"});
-    $(this).parent(".grid__row").children(":nth-child(4)").css({"margin-top": "-=5px"});
-    $(this).parent(".grid__row").children(":nth-child(5)").css({"margin-top": "-=0px"});
+    $(this).parent(".grid__row").children(":nth-child(2)").css({"margin-top": "-75px"});
+    $(this).parent(".grid__row").children(":nth-child(3)").css({"margin-top": "-25px"});
+    $(this).parent(".grid__row").children(":nth-child(4)").css({"margin-top": "-5px"});
+    $(this).parent(".grid__row").children(":nth-child(5)").css({"margin-top": "-0px"});
   }
 
   else if(index == 2) {
-    $(this).parent(".grid__row").children(":nth-child(1)").css({"margin-top": "-=75px"});
-    $(this).parent(".grid__row").children(":nth-child(3)").css({"margin-top": "-=75px"});
-    $(this).parent(".grid__row").children(":nth-child(4)").css({"margin-top": "-=25px"});
-    $(this).parent(".grid__row").children(":nth-child(5)").css({"margin-top": "-=5px"});
+    $(this).parent(".grid__row").children(":nth-child(1)").css({"margin-top": "-75px"});
+    $(this).parent(".grid__row").children(":nth-child(3)").css({"margin-top": "-75px"});
+    $(this).parent(".grid__row").children(":nth-child(4)").css({"margin-top": "-25px"});
+    $(this).parent(".grid__row").children(":nth-child(5)").css({"margin-top": "-5px"});
   }
 
   else if(index == 3) {
-    $(this).parent(".grid__row").children(":nth-child(1)").css({"margin-top": "-=25px"});
-    $(this).parent(".grid__row").children(":nth-child(2)").css({"margin-top": "-=75px"});
-    $(this).parent(".grid__row").children(":nth-child(4)").css({"margin-top": "-=75px"});
-    $(this).parent(".grid__row").children(":nth-child(5)").css({"margin-top": "-=25px"});
+    $(this).parent(".grid__row").children(":nth-child(1)").css({"margin-top": "-25px"});
+    $(this).parent(".grid__row").children(":nth-child(2)").css({"margin-top": "-75px"});
+    $(this).parent(".grid__row").children(":nth-child(4)").css({"margin-top": "-75px"});
+    $(this).parent(".grid__row").children(":nth-child(5)").css({"margin-top": "-25px"});
   }
 
   else if(index == 4) {
-    $(this).parent(".grid__row").children(":nth-child(1)").css({"margin-top": "-=5px"});
-    $(this).parent(".grid__row").children(":nth-child(2)").css({"margin-top": "-=25px"});
-    $(this).parent(".grid__row").children(":nth-child(3)").css({"margin-top": "-=75px"});
-    $(this).parent(".grid__row").children(":nth-child(5)").css({"margin-top": "-=75px"});
+    $(this).parent(".grid__row").children(":nth-child(1)").css({"margin-top": "-5px"});
+    $(this).parent(".grid__row").children(":nth-child(2)").css({"margin-top": "-25px"});
+    $(this).parent(".grid__row").children(":nth-child(3)").css({"margin-top": "-75px"});
+    $(this).parent(".grid__row").children(":nth-child(5)").css({"margin-top": "-75px"});
   }
 
   else if(index == 5) {
-    $(this).parent(".grid__row").children(":nth-child(1)").css({"margin-top": "-=0px"});
-    $(this).parent(".grid__row").children(":nth-child(2)").css({"margin-top": "-=5px"});
-    $(this).parent(".grid__row").children(":nth-child(3)").css({"margin-top": "-=25px"});
-    $(this).parent(".grid__row").children(":nth-child(4)").css({"margin-top": "-=75px"});
+    $(this).parent(".grid__row").children(":nth-child(1)").css({"margin-top": "-0px"});
+    $(this).parent(".grid__row").children(":nth-child(2)").css({"margin-top": "-5px"});
+    $(this).parent(".grid__row").children(":nth-child(3)").css({"margin-top": "-25px"});
+    $(this).parent(".grid__row").children(":nth-child(4)").css({"margin-top": "-75px"});
   }
 
 });
 
-
+//Collective jitter of content
 $( ".grid__row" ).mouseover(function() {
   var jitter = getRandomInt(1,10);
-
   if (jitter%2 == 0) {
-    $(".grid__row").css({"margin-right": "-=35px"});
+    $(".grid__row").css({"margin-right": "-35px"});
   }
   else {
-    $(".grid__row").css({"margin-left": "-=35px"});
+    $(".grid__row").css({"margin-left": "-35px"});
   }
-
 });
 
-
-$( ".grid__rect" ).mouseout(function() {
-  $(".grid__rect").css({"margin-top": 0});
-  $(".grid__row").css({"margin-right": 0,
-                      "margin-left": 0});
-});
-
-
+//Background images for projects
 $( ".grid__rect--gif").click(function() {
   $(this).find(".grid__rect-inner").css({"background-image": "url('../assets/emojis/" + getRandomInt(1,33) + ".gif')"});
+});
+
+$( ".grid__rect--game").click(function() {
+  $(this).find(".grid__rect-inner").css({"background-image": "url('../assets/gifs/game-" + getRandomInt(1,4) + ".gif')"});
 });
 
 
 //Draggable UI
 //-
 $( function() {
-  $('.grid__rect').draggable();
-} );
+  // $(".grid__rect-inner").resizable();
+
+  $(".grid__rect").draggable({
+    appendTo: 'body',
+    start: function(event, ui) {
+        isDraggingMedia = true;
+        $(".grid__rect").css({"cursor": "-webkit-grabbing"});
+    },
+    stop: function(event, ui) {
+        isDraggingMedia = false;
+        $(".grid__rect").css({"cursor": "-webkit-grab"});
+    }
+  });
+});
