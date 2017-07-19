c36ed2118ac840bd983b45109820d7b0d0308c7d
Nathalie de Valliere <nathalie.devalliere@gmail.com>
2017-07-19 10:01:10 -0400

    fixing path links

--- scripts/scripts-10.js
+++ scripts/scripts-10.js
@@ -1,13 +1,45 @@
+var triggerAbout = true,
+    triggerEvents = true;
+
 $("#cognateBlack").click(function() {
     $("#animateBlack").attr("begin", 0);
     $(".link__page").css({"cursor": "pointer"});
     $(".link__page--logo").css({"cursor": "url('../assets/png/gasp.png'), pointer"});
 });
 
-$(".link__page--nav").mouseover(function() {
-    $(this).css({"transform": "rotateY(180deg)"});
+$(".link__page--nav-about").click(function() {
+    triggerAbout = false;
+    $(this).css({"padding-right": "100%",
+                 "transform": "rotateY(180deg)",
+                 "background-color": "white"});
+    $(".link__page--nav-about").css({"padding-right": 0});
 });
 
-$(".link__page--nav").mouseout(function() {
+$(".link__page--nav-events").click(function() {
+    triggerEvents = false;
+    $(this).css({"padding-right": "100%",
+                 "transform": "rotateY(180deg)",
+                 "background-color": "white"});
+});
+
+$(".link__page--nav-about").mouseover(function() {
+  if (triggerAbout) {
+    $(this).css({"transform": "rotateY(180deg)"});
+  }
+});
+$(".link__page--nav-about").mouseout(function() {
+  if (triggerAbout) {
     $(this).css({"transform": "rotateY(0deg)"});
+  }
+});
+
+$(".link__page--nav-events").mouseover(function() {
+  if (triggerEvents) {
+    $(this).css({"transform": "rotateY(180deg)"});
+  }
+});
+$(".link__page--nav-events").mouseout(function() {
+  if (triggerEvents) {
+    $(this).css({"transform": "rotateY(0deg)"});
+  }
 });
