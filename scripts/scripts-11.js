c3240539332fc64a9daea22322edd78d66c0b319
Nathalie de Valliere <nathalie.devalliere@gmail.com>
2017-07-26 21:56:50 -0400

    animating lines

---
+++ scripts/scripts-11.js
@@ -0,0 +1,19 @@
+var resize = (function () {
+		var IH = window.innerHeight,
+				IW = window.innerWidth;
+
+		$("container__svg").css({"height": IH, "width": IW});
+
+  });
+
+$(document).ready(resize);
+$(window).resize(resize);
+
+
+$("path").mouseover(function() {
+    $(this).css({"fill": "rgba(255,255,255,1)");
+});
+
+$("path").mouseover(function() {
+    $(this).css({"fill": "rgba(255,255,255,0)"});
+});
