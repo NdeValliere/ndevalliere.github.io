5b52b873eed8e17d8b301d4aa78222b3e3a3848a
Nathalie de Valliere <nathalie.devalliere@gmail.com>
2017-12-14 23:07:37 +0100

    new python page

---
+++ python/js/script.js
@@ -0,0 +1,10 @@
+var IH = window.innerHeight,
+    IW = window.innerWidth;
+
+$("#container__background-img").css({"height": IH});
+// $("#container__background-img").css({"width": IW});
+
+$("#container__background").css({"height": IH});
+$("#container__background").css({"width": IW});
+
+console.log("height: " + IH + " width: " + IW);
