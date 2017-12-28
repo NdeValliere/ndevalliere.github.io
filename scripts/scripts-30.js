b86a3866af83388a27994c16efdac8c1cdef81d0
Nathalie de Valliere <nathalie.devalliere@gmail.com>
2017-12-28 11:39:14 +0100

    1st episode part 1 draft 1

---
+++ scripts/scripts-30.js
@@ -0,0 +1,59 @@
+//Math tools
+//-
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+//Window resizing tools
+//-
+var IH = window.innerHeight,
+    IW = window.innerWidth;
+var resize = (function () {
+		$(".container__grid").css({"width": IW *0.9,
+                               "height": IH});
+
+    $(".grid__col").css({"height": IH});
+    $(".grid__col--12").css({"max-width": IW*0.9});
+
+    $(".quote--center").css({"padding-top": IH/2});
+    $("img").css({"height": IH});
+
+
+    // var img = document.getElementById("img__center");
+    // img.onload = function() {
+    //   $("#img__center").css({"padding-left": (IW - img.width)/2,
+    //                          "padding-right": (IW - img.width)/2});
+    //   // console.log((IW - img.width)/2);
+    // }
+});
+
+$(document).ready(resize);
+$(window).resize(resize);
+
+//Click-through code
+//-
+$(document).ready(function() {
+  //hide content before cycling through it onclick
+  for (var i = 2; i < 50; i++) {
+    $(".container__grid--"+ i).css({"left": IW*0.9,
+                                    "visibility": "hidden"});
+  }
+
+  var counter = 1;
+  $( "html" ).click(function() {
+      counter++;
+      $(".container__grid--"+ (counter-1) ).css({ "left": -IW*0.9});
+      $(".quote--"+ (counter-1) ).css({"margin-top": -IH});
+      $(".container__grid--"+ counter+1 ).css({"visibility": "visible"});
+      $(".container__grid--"+ counter ).css({"left": 0,
+                                              "visibility": "visible"});
+      // console.log("click" + counter);
+      $("body").css({"cursor": "url('../assets/png/clickHere-" + getRandomInt(1,5) + ".png'), auto"});
+  });
+});
+
+// $(window).load(function(){
+//   $("#test").css({"padding-left": (IW - ($("#test").width))/2,
+//                   "padding-right": (IW - ($("#test").width))/2});
+//   console.log($("#test").width);
+// });
