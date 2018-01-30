5f77b2e4196c868482060f5d083df8f7d5284317
ndevalliere <ndevalliere@gmail.com>
2018-01-30 13:50:27 +0000

    mass edits + beta version of new portfolio

---
+++ scripts/scriptsPortfolio03.js
@@ -0,0 +1,9 @@
+// $(document).ready(function() {
+// 	$('#fullpage').fullpage();
+// 	});
+
+
+//Get position of fullpage brows to position links (this might be weird)
+var brow = $("div:first");
+var positionBrow = brow.position();
+console.log(positionBrow.top + " is current position of aboutBrow");
