19154a64a153b1eb7748815f7be4a4a97db29043
ndevalliere <ndevalliere@gmail.com>
2018-02-05 09:01:17 +0000

    Adding reactions to links

--- scripts/scriptsPortfolio05.js
+++ scripts/scriptsPortfolio05.js
@@ -1,4 +1,4 @@
-//Window resizing tools
+//Window resize content - fuck pixel perfection, aint nobody be able to control everything
 //-
 $(document).ready(resize);
 $(window).resize(resize);
@@ -9,12 +9,12 @@ function resize() {
 		$(".container__grid").css({"width": IW });
     $(".container__process").css({"width": IW });
     $(".process__bar").css({"width": IW/6 * 1 });
+    $(".td__border").css({"width": 2*(IW/12) + "px"});
+    $(".td__img").css({"height": 2*(IW/16,
+                       "margin-bottom": )});
 
    for( var i = 0; i < 13; i++) {
-       $(".grid__col--"+ i ).css({"width": (IW/12)*i + "px"});
-        $(".td__border").css({"width": 2*(IW/12) + "px",
-                              "height":  $(".td__border").css("width")
-                            });
+      $(".grid__col--"+ i ).css({"width": (IW/12)*i + "px"});
       $(".td__space").css({"width": ($(".container__preview-imgs").innerWidth() - (4 * (IW/12)))/6 });
       $(".tr__space").css({"height": ($(".container__preview-imgs").innerWidth() - (4 * (IW/12)))/12 });
       $(".container__grid--"+ i ).css({"position" : "absolute",
@@ -22,10 +22,12 @@ function resize() {
                                         "margin-bottom" : (IH - $(".container__grid--"+ i ).innerHeight()) /2
                                       });
    }
-   $(".container__grid--1" ).css({"opacity" : 1 });
+   $(".container__grid--1" ).css({"opacity" : 1,
+                                  "z-index": 20
+                                  });
 }
 
-//Click-through code
+//Scroll-through code for slideshow-ish transitions
 //-
 $(document).ready(function() {
   var counter = 1,
@@ -46,11 +48,13 @@ $(document).ready(function() {
           $(".process__bar").css({"width": ($(window).innerWidth() / 6) * (counter) });
 
           $(".container__grid--"+ (counter)).css({"opacity": "1",
-                                                    "margin-top" :  $(".container__grid--"+ (counter+1)).css("margin-bottom")
+                                                  "margin-top" :  $(".container__grid--"+ (counter+1)).css("margin-bottom"),
+                                                  "z-index": 20
                                                   });
           $(".container__grid--"+ (counter+1)).css({"opacity": "0",
                                                     "padding-top": 4.15 + "%",
-                                                    "margin-top" : $(".container__grid--"+ (counter+2)).css("margin-bottom")
+                                                    "margin-top" : $(".container__grid--"+ (counter+2)).css("margin-bottom"),
+                                                    "z-index": -1
                                                     // "border": "10px solid red"
                                                     });
       }
@@ -65,11 +69,35 @@ $(document).ready(function() {
         $(".process__bar").css({"width": ($(window).innerWidth() / 6) * (counter+1) });
 
         $(".container__grid--"+ (counter)).css({"opacity": "0",
-                                                "margin-top" : -1 * ($(".container__grid--" + counter).innerHeight() + (($(window).innerHeight() - $(".container__grid--" + counter).innerHeight())/2))
+                                                "margin-top" : -1 * ($(".container__grid--" + counter).innerHeight() + (($(window).innerHeight() - $(".container__grid--" + counter).innerHeight())/2)),
+                                                "z-index": -1
                                                 });
         $(".container__grid--"+ (counter+1)).css({"opacity": "1",
-                                                  "padding-top": 0
+                                                  "padding-top": 0,
+                                                  "z-index": 20
                                                 });
       }
+      if (counter==6) {
+        $(".container__down").css({"cursor": "default"});
+      }
+      else {
+        $(".container__down").css({"cursor": "s-resize"});
+      }
   });
+});
+
+//Link hover animations
+//-
+$(".link__out").mouseover(function() {
+  $(this).toggleClass("hover");
+});
+
+$(".link__out").mouseout(function() {
+  $(this).toggleClass("hover");
+});
+
+//Image link hover animations
+//-
+$(".td__img").mouseover(function() {
+  // $(this).css({"padding-top": ($(this).innerHeight())/2});
 });
