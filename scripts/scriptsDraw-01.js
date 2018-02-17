aafc736d96e13c5e770b8d23adf0c56f25046392
leonardopellegrino <pellegrino.graphic@gmail.com>
2018-02-17 15:51:31 +0000

    Adding new pages, hosting content externally

---
+++ scripts/scriptsDraw-01.js
@@ -0,0 +1,11 @@
+//Window resize content - fuck pixel perfection, aint nobody be able to control everything
+//-
+$(document).ready(resize);
+$(window).resize(resize);
+
+function resize() {
+  var IH = window.innerHeight,
+      IW = window.innerWidth;
+		$(".container__grid").css({"width": IW
+                                      });
+}
